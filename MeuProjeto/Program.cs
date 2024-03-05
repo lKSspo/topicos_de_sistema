5f1f0f0bd79b0e709031963bd872eab9516e408d
lKSspo <133040087+lKSspo@users.noreply.github.com>
2024-03-05 13:19:15 -0300

    Add files via upload

---
+++ MeuProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MeuProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
